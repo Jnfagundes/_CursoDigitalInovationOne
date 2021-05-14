a799833aef94dbe006bd8b2e8e69422f369f2e43
Jnfagundes <jasfagundesmusic@gmail.com>
2021-05-14 00:28:03 -0300

    Novas funções no js

--- js/man.js
+++ js/man.js
@@ -1,24 +1,24 @@
 
- let nome = "José Nilton";
-alert("Bem vindo " + nome); //O sinal de + está concatenando a string com a variável
+// let nome = "José Nilton";
+//alert("Bem vindo " + nome); //O sinal de + está concatenando a string com a variável
 
-var lista = ["maçã", "pêra", "uva"]; /*Criação de uma estrutura do tipo lista*/
-lista.push("laranja"); /*Com esse comando acrescentamos itens na lista*/
+//var lista = ["maçã", "pêra", "uva"]; /*Criação de uma estrutura do tipo lista*/
+//lista.push("laranja"); /*Com esse comando acrescentamos itens na lista*/
 //lista.pop(); /*Com esse comando excluimos o ultimo item da lista*/
-console.log(lista); /*Apresentação dessa estrutura dentro do console*/
-console.log(lista.length); //Esse comando mostra o tamanho da lista.
-console.log(lista.reverse()); // Esse comando mostra a lista de forma inversa.
-console.log(lista.toString()); // Mostra a lista como string
-console.log(lista.join("  -  ")); //Faz a mesma coisa do toString, porém permite escolher o que vai separar cada item
-alert(lista); /*Apresentação dessa estrutura dentro do navegador*/
+//console.log(lista); /*Apresentação dessa estrutura dentro do console*/
+//console.log(lista.length); //Esse comando mostra o tamanho da lista.
+//console.log(lista.reverse()); // Esse comando mostra a lista de forma inversa.
+//console.log(lista.toString()); // Mostra a lista como string
+//console.log(lista.join("  -  ")); //Faz a mesma coisa do toString, porém permite escolher o que vai separar cada item
+//alert(lista); /*Apresentação dessa estrutura dentro do navegador*/
 
-var fruta = {nome:"maçã", cor:"vermelha"}; //Criação do discionário
-console.log(fruta); // Apresentação dentro do console
-alert(fruta.nome); //Apresentação dentro da página
+//var fruta = {nome:"maçã", cor:"vermelha"}; //Criação do discionário
+//console.log(fruta); // Apresentação dentro do console
+//alert(fruta.nome); //Apresentação dentro da página
 
-let idade = prompt("Qual a sua idade? "); //Esse comando pergunta, via página, qual a idade do usuário
+//let idade = prompt("Qual a sua idade? "); //Esse comando pergunta, via página, qual a idade do usuário
 
-if (idade >= 18) {
+/*if (idade >= 18) {
     alert("Maior de idade");
 } else {
     alert("menor de idade");
@@ -37,4 +37,29 @@ for (contador = 0; contador <= 5; contador++) {
 }
 
 let d = new Date();
-alert(d);
+alert(d);*/
+/*
+function soma(n1,n2) {
+    return n1+n2;
+    
+}
+
+alert(soma(10,20));
+
+function validaIdade(idade) {
+    var validar;
+    if (idade >= 18) {
+        validar = true;
+    } else {
+        validar = false;
+    }
+    return validar;
+}
+
+var idade = prompt("Qual a sua idade? ");
+console.log(validaIdade(idade))
+*/
+
+function botao(){
+    alert("Obrigado por ter clicado")
+}
